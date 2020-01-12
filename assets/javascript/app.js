588d9890f577614b34a807fc0faea9ef1fb4791e
Jeff.A <kinesthesiac@gmail.com>
2020-01-12 11:59:13 -0700

    refactored code to better adhear to the directions pertaining to using JS for logic and JQ for hmtl manipulation.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,26 @@
 //timer counting down in seconds
-    //javascript timer function
+//javascript timer function
 
 var wins = 0
 var losses = 0
 
-var seconds = document.getElementById("gameStatePrompt").textContent;
+// var seconds = document.getElementById("gameStatePrompt").textContent;
+var seconds = $("#gameStatePrompt").text();
+
 var countdown = setInterval(function() {
     seconds--;
-    document.getElementById("gameStatePrompt").textContent = seconds;
+    //document.getElementById("gameStatePrompt").textContent = seconds;
+    $("#gameStatePrompt").text(seconds);
+    console.log("TYPE: ", typeof seconds)
     if (seconds < 0) {
         clearInterval(countdown);
         check1()
         var score = confirm("Time's Up!  Correct Answers: " + wins + " Incorrect Answer: " + losses);
         console.log("time's up")
-        
+
+
         function check1() {
-            if(document.getElementById('answer1B').checked) {           
+            if (document.getElementById('answer1B').checked) {
                 console.log("correct answer detected")
                 wins = wins + 1
             } else {
@@ -23,7 +28,7 @@ var countdown = setInterval(function() {
                 losses = losses + 1
             }
 
-            if(document.getElementById('answer2C').checked) {           
+            if (document.getElementById('answer2C').checked) {
                 console.log("correct answer detected")
                 wins = wins + 1
             } else {
@@ -31,7 +36,7 @@ var countdown = setInterval(function() {
                 losses = losses + 1
             }
 
-            if(document.getElementById('answer3A').checked) {           
+            if (document.getElementById('answer3A').checked) {
                 console.log("correct answer detected")
                 wins = wins + 1
             } else {
@@ -39,7 +44,7 @@ var countdown = setInterval(function() {
                 losses = losses + 1
             }
 
-            if(document.getElementById('answer4A').checked) {           
+            if (document.getElementById('answer4A').checked) {
                 console.log("correct answer detected")
                 wins = wins + 1
             } else {
@@ -47,7 +52,7 @@ var countdown = setInterval(function() {
                 losses = losses + 1
             }
 
-            if(document.getElementById('answer5D').checked) {           
+            if (document.getElementById('answer5D').checked) {
                 console.log("correct answer detected")
                 wins = wins + 1
             } else {
@@ -55,7 +60,7 @@ var countdown = setInterval(function() {
                 losses = losses + 1
             }
 
-            if(document.getElementById('answer6B').checked) {           
+            if (document.getElementById('answer6B').checked) {
                 console.log("correct answer detected")
                 wins = wins + 1
             } else {
@@ -63,20 +68,17 @@ var countdown = setInterval(function() {
                 losses = losses + 1
             }
 
-            if(document.getElementById('answer7A').checked) {           
+            if (document.getElementById('answer7A').checked) {
                 console.log("correct answer detected")
                 wins = wins + 1
             } else {
                 console.log("incorrect answer detected")
                 losses = losses + 1
             }
-
-
-
 
         };
 
-        
+
     };
 }, 1000);
 
@@ -90,30 +92,23 @@ var countdown = setInterval(function() {
 
 
 
-    //if timer reaches 0 the game ends
+//if timer reaches 0 the game ends
 
 //promt the user to start the game
-    //button click calls the timer function
+//button click calls the timer function
 
 // update html to show the countdown happening
-    //jq continually shows the current count
-    //jq removes the interface and show a score in it's place when the game is over
+//jq continually shows the current count
+//jq removes the interface and show a score in it's place when the game is over
 
 
 
 //wire up the buttons to show the answer
-    //limit user to one answer per question
+//limit user to one answer per question
 
 
 //scoring
-    //at the end of the game:
-        //check for correct answers and count them
-        //check for incorrect answers and count them
-        //update score card to show incorrect and correct answer scores
-
-
-
-
-
-
-
+//at the end of the game:
+//check for correct answers and count them
+//check for incorrect answers and count them
+//update score card to show incorrect and correct answer scores
