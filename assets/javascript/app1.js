3734942bde7aeb67eac8bcafc4b7185bf51aa951
Jeff.A <kinesthesiac@gmail.com>
2020-02-28 00:51:07 -0700

    added styling, dynamic images, and a reward screen.

--- assets/javascript/app1.js
+++ assets/javascript/app1.js
@@ -6,39 +6,79 @@ var losses = 0
 
 // var seconds = document.getElementById("gameStatePrompt").textContent;
 var seconds = $("#gameStatePrompt").text();
+var xeconds = 2
+
 var backgrounds = [
-    "../images/crowdsurfing.jpg",
-    "../images/nin.png",
-    "../images/beck.jpg",
-    "../images/REM.jpg",
-    "../images/zappa.jpeg",
-    "../images/soundgarden.jpg",
-    "../images/trent.jpg",
-    "../images/radiohead",
-    "../images/tom-morello.jpg",
-    "../images/lateralis.jpg",
-    "../images/GNR.jpg",
-    "../images/janesaddiction.jpg",
-    "../images/tool.jpg",
-    "../images/beavis-and-butthead-nin.jpg",
-    "../images/aliceInChains.jpg",
-    "../images/lateralis2.png",
-    "../images/trent2.jpg",
-    "../images/Rage-Against-the-Machine.jpg",
-    "../images/frank.jpg",
-    "../images/kurtd.jpg",
+    "../assets/images/crowdsurfing.jpg",
+    "../assets/images/nin.png",
+    "../assets/images/beck.jpg",
+    "../assets/images/REM.jpg",
+    "../assets/images/zappa.jpeg",
+    "../assets/images/soundgarden.jpg",
+    "../assets/images/trent.jpg",
+    "../assets/images/radiohead.jpg",
+    "../assets/images/tom-morello.jpg",
+    "../assets/images/lateralis.jpg",
+    "../assets/images/GNR.jpg",
+    "../assets/images/janesaddiction.jpg",
+    "../assets/images/tool.jpg",
+    "../assets/images/beavis-and-butthead-nin.jpg",
+    "../assets/images/aliceInChains.jpg",
+    "../assets/images/lateralis2.png",
+    "../assets/images/trent2.jpg",
+    "../assets/images/Rage-Against-the-Machine.jpg",
+    "../assets/images/frank.jpg",
+    "../assets/images/kurtd.jpg",
+    
+    "../assets/images/crowdsurfing.jpg",
+    "../assets/images/nin.png",
+    "../assets/images/beck.jpg",
+    "../assets/images/REM.jpg",
+    "../assets/images/zappa.jpeg",
+    "../assets/images/soundgarden.jpg",
+    "../assets/images/trent.jpg",
+    "../assets/images/radiohead.jpg",
+    "../assets/images/tom-morello.jpg",
+    "../assets/images/lateralis.jpg",
+    "../assets/images/GNR.jpg",
+    "../assets/images/janesaddiction.jpg",
+    "../assets/images/tool.jpg",
+    "../assets/images/beavis-and-butthead-nin.jpg",
+    "../assets/images/aliceInChains.jpg",
+    "../assets/images/lateralis2.png",
+    "../assets/images/trent2.jpg",
+    "../assets/images/Rage-Against-the-Machine.jpg",
+    "../assets/images/frank.jpg",
+    "../assets/images/kurtd.jpg"
+
 ]
 
 var countdown = setInterval(function() {
     seconds--;
     //document.getElementById("gameStatePrompt").textContent = seconds;
     $("#gameStatePrompt").text(seconds);
+    $("#bGPics").css('background-image', "url(" + backgrounds[seconds] + ")");
+    console.log(backgrounds[seconds])
     console.log("TYPE: ", typeof seconds)
     if (seconds < 0) {
         clearInterval(countdown);
         check1()
-        var score = confirm("Time's Up!  Correct Answers: " + wins + " Incorrect Answer: " + losses);
-        console.log("time's up")
+        if (confirm("Time's Up!  Correct Answers: " + wins + " Incorrect Answer: " + losses)) {
+            $(".gameboard").empty();
+            $("#bGPics").css('background-image', 'url("../assets/images/kyuss.jpg")');
+            var countdownAgain = setInterval(function() {
+                xeconds--;
+                if (xeconds < 0) {
+                    clearInterval(countdownAgain);
+                    $(".instructions").empty();
+                    $(".instructions").css("color", "black")
+                    $(".instructions").css("text-shadow", "1px 1px 2px orangered")
+                    $(".instructions").html("<br><br><br><br><h3>Thanks for playing.  Take with you this delicious morsel of 90’s music lore trivia:  The icon of this era, Nirvana, is credited with inventing the Seattle Sound (otherwise known as Grunge).  However this isn’t exactly true.  Many argue that Joshua Homme did so with his band, Kyuss, in Palm Desert, California in the late 80’s.  Kyuss enjoyed only a small cultish fame.  <br> People would gather for huge, drug-fueled concert parties in remote locations deep in the desert wilderness; something like a primitive version of Burning Man.  The concerts would last for literally days without a break.  In this setting bands were expected to play for hours.  What emerged was something incorporating early metal combined with the long, meandering forms of jam bands like the Phish or Widespread Panic.  Kyuss grew to dominate this scene, and then eventually disbanded without ever achieving mainstream popularity.  <br>  Seattle bands discovered this sound and made it wildly popular.  The 90’s came and went, and the popularity of this style faded.  <br>  Later Joshua Homme founded a new band, Queens of the Stone Age, which is much more successful than Kyuss was.  The name of this band is actually a brief description of Homme’s misplacement in this history.  The Stone Age is, is 90’s grunge.  And while he was never the king, his bands were always the Queens behind the scenes.")
+                }
+
+
+            }, 1000)
+        }
         //set up rotating an random images off the timer here
         
 
